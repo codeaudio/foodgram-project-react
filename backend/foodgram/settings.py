f940531dd96bfb1f81df9784400d1b24ccf141fc
Иван <ivan07m@gmail.com>
2022-01-11 05:44:38 +0300

    'fix after review'

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/staticdjango/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticdjango')
-MEDIA_URL = '/mediadjango/'
+MEDIA_URL = "/mediadjango/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediadjango')
 
 CORS_ORIGIN_ALLOW_ALL = True
