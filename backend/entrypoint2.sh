89e4ba66d38e7d5b4d805f24c77d6877b27a24c4
Иван <ivan07m@gmail.com>
2022-01-11 04:41:13 +0300

    'fix after review'

--- backend/entrypoint2.sh
+++
@@ -1,1 +0,0 @@
-exec  gunicorn foodgram.wsgi:application -c 0.0.0.0:8000
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,11 +95,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', default='postgres'),
+        'USER': os.environ.get('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.environ.get('DB_HOST', default='localhost'),
+        'PORT': os.environ.get('DB_PORT', default="5432")
     }
 }
 
