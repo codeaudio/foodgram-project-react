1f19d7223534a6f94ec95acdc4757032c53cfdeb
Иван <ivan07m@gmail.com>
2022-01-14 14:07:41 +0300

    'fix after review'

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,7 @@ from .models import Recipe, RecipeFavorite, ShoppingList, Ingredient
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_class='tags__slug')
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     author = filters.Filter(field_name='author__id')
     is_favorited = django_filters.NumberFilter(
         method='filter_is_favorited',
