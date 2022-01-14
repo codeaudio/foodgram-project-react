63abf6e83a4261b96e1b7b34be409570bbbf353b
Иван <ivan07m@gmail.com>
2022-01-14 12:57:48 +0300

    'fix after review'

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,7 @@ from .models import Recipe, RecipeFavorite, ShoppingList, Ingredient
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.CharFilter(field_name='tags__slug')
+    tags = filters.CharFilter(field_name='tags__slug', lookup_expr='exact')
     author = filters.Filter(field_name='author__id')
     is_favorited = django_filters.NumberFilter(
         method='filter_is_favorited',
