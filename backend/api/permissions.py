57fddcba9253ffc8f379f4dce6bdf235559200af
Иван <ivan07m@gmail.com>
2022-01-16 21:11:34 +0300

    'fix after review'

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class OwnerPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.author
+
+
+class IsNotSelfPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user != obj.author
